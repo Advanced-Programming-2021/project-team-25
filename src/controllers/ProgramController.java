e5918403b36411314de1df2220ceeedace87b4fc
noroo <noroozi.alirezaa@gmail.com>
2021-04-15 17:21:15 +0430

    commit

--- src/controllers/ProgramController.java
+++ src/controllers/ProgramController.java
@@ -10,7 +10,7 @@ public class ProgramController {
     public static Scanner scanner = new Scanner(System.in);
 
     public void run() {
-        while ( currentMenu != Menu.EXIT ) {
+        while (currentMenu != Menu.EXIT) {
             String command = scanner.nextLine();
             if (Regex.getMatcher(command, Regex.menuShowCurrent).matches()) System.out.println(currentMenu);
 
@@ -18,16 +18,14 @@ public class ProgramController {
 
             else if (currentMenu == Menu.MAIN_MENU) {
                 new MainMenu().runMainMenu(command);
-                if ( currentMenu == Menu.DUEL_MENU ) new DuelMenu().runDuelMenu(command);
-                else if ( currentMenu == Menu.DECK_MENU ) new DeckMenu().runDeckMenu(command);
-                else if ( currentMenu == Menu.SHOP_MENU ) new ShopMenu().runShopMenu(command);
-                else if ( currentMenu == Menu.PROFILE_MENU ) new ProfileMenu().runProfileMenu(command);
-            }
-
-            else if (currentMenu == Menu.DECK_MENU)  new DeckMenu().runDeckMenu(command);
-            else if (currentMenu == Menu.SHOP_MENU)  new ShopMenu().runShopMenu(command);
-            else if (currentMenu == Menu.DUEL_MENU)  new DuelMenu().runDuelMenu(command);
-            else if (currentMenu == Menu.PROFILE_MENU)  new ProfileMenu().runProfileMenu(command);
+                if (currentMenu == Menu.DUEL_MENU) new DuelMenu().runDuelMenu(command);
+                else if (currentMenu == Menu.DECK_MENU) new DeckMenu().runDeckMenu(command);
+                else if (currentMenu == Menu.SHOP_MENU) new ShopMenu().runShopMenu(command);
+                else if (currentMenu == Menu.PROFILE_MENU) new ProfileMenu().runProfileMenu(command);
+            } else if (currentMenu == Menu.DECK_MENU) new DeckMenu().runDeckMenu(command);
+            else if (currentMenu == Menu.SHOP_MENU) new ShopMenu().runShopMenu(command);
+            else if (currentMenu == Menu.DUEL_MENU) new DuelMenu().runDuelMenu(command);
+            else if (currentMenu == Menu.PROFILE_MENU) new ProfileMenu().runProfileMenu(command);
         }
     }
 }
