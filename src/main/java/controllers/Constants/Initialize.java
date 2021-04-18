159a5d9b21f0300270c03c9490ea448a782adee2
Pouria-Empire <arefijamal.pouria@gmail.com>
2021-04-18 19:03:19 +0430

    First login bug solved :)

--- src/main/java/controllers/Constants/Initialize.java
+++ src/main/java/controllers/Constants/Initialize.java
@@ -3,9 +3,16 @@ package controllers.Constants;
 import controllers.Database.DataBase;
 import models.User;
 
+import java.io.File;
+
 public class Initialize {
 
     public static void init() {
-        User.setUsers(DataBase.loadTheList());
+        initUserList();
+    }
+    public static void initUserList(){
+        File tmpDir = new File("savedList.list");
+        if(tmpDir.exists()) User.setUsers(DataBase.loadTheList());
+        else new User("admin","admin","Game");
     }
 }
--- src/main/java/controllers/Database/DataBase.java
+++ src/main/java/controllers/Database/DataBase.java
@@ -15,6 +15,7 @@ public class DataBase {
             ObjectOutputStream oos = new ObjectOutputStream(fileOut);
             oos.writeObject(users);
             oos.close();
+            fileOut.flush();
         } catch (Exception e) {
             e.printStackTrace();
         }
