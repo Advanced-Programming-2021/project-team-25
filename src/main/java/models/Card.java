8b6d690b2fb0d40043cb3a43e3dc5c58981c4164
nazari <X.LordAliX@yahoo.com>
2021-04-19 21:29:14 +0430

    card class and its enums and monster and spell and trap class added

--- src/main/java/models/Card.java
+++ src/main/java/models/Card.java
@@ -1,6 +1,75 @@
 package models;
 
-public class Card {
-    private String name;
+import models.CardStufs.FaceUp;
+import models.CardStufs.Location;
+import models.CardStufs.Type;
 
+import java.util.HashMap;
+
+public class Card {
+    public static HashMap<String, Card> allCards = new HashMap<>();
+    private String name;
+    private Type cardsType;
+    private FaceUp cardsFace;
+    private Location cardsLocation;
+    private boolean setChanged;
+    private boolean isSetThisTurn;
+    private String description;
+    private int price;
+
+    public Card(String name, Type cardsType, String description, int price) {
+        this.name = name;
+        this.cardsType = cardsType;
+        this.description = description;
+        this.price = price;
+        allCards.put(name, this);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getCardsType() {
+        return cardsType;
+    }
+
+    public FaceUp getCardsFace() {
+        return cardsFace;
+    }
+
+    public void setCardsFace(FaceUp cardsFace) {
+        this.cardsFace = cardsFace;
+    }
+
+    public Location getCardsLocation() {
+        return cardsLocation;
+    }
+
+    public void setCardsLocation(Location cardsLocation) {
+        this.cardsLocation = cardsLocation;
+    }
+
+    public boolean getSetChanged() {
+        return setChanged;
+    }
+
+    public void setSetChanged(boolean trueOrFalse) {
+        this.setChanged = trueOrFalse;
+    }
+
+    public boolean getIsSetThisTurn() {
+        return isSetThisTurn;
+    }
+
+    public void setIsSetThisTurn(boolean trueOrFalse) {
+        this.isSetThisTurn = trueOrFalse;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public int getPrice() {
+        return price;
+    }
 }
---
+++ src/main/java/models/SpellAndTrap/SpellAndTrap.java
@@ -0,0 +1,33 @@
+package models.SpellAndTrap;
+
+import models.Card;
+import models.CardStufs.Type;
+
+public class SpellAndTrap extends Card {
+
+    private String icon;
+    private String status;
+
+    public SpellAndTrap(String name, Type cardType, String description, int price, String icon, String status) {
+        super(name, cardType, description, price);
+        this.icon = icon;
+        this.status = status;
+    }
+
+    //this is copy constructor of SpellAndTrap class
+    public SpellAndTrap(Object object) {
+        super(((SpellAndTrap) object).getName(), ((SpellAndTrap) object).getCardsType(), ((SpellAndTrap) object).getDescription(),
+                ((SpellAndTrap) object).getPrice());
+        SpellAndTrap spellAndTrap = (SpellAndTrap) object;
+        this.icon = spellAndTrap.icon;
+        this.status = spellAndTrap.status;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
