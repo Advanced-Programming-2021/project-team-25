83d21540b175424773be309983a8ddaeaa78f1cf
Pouria-Empire <arefijamal.pouria@gmail.com>
2021-04-19 16:03:08 +0430

    Hi Card list Of Monsters Based

---
+++ src/main/java/models/Cards/Card.java
@@ -0,0 +1,11 @@
+package models.Cards;
+
+public class Card {
+    private String name;
+    private Position position;
+    private Type type;
+    private boolean setChanged;
+    private boolean isSetThisTurn;
+
+
+}
