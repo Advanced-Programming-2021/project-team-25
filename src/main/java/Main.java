743745943f58d36aaef1dda1bf8e9958ae64765e
Pouria-Empire <arefijamal.pouria@gmail.com>
2021-04-23 16:07:02 +0430

    changed to be run

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,6 @@ import controllers.ProgramController;
 
 public class Main {
     public static void main(String[] args) {
-        new ProgramController().run();
+        ProgramController.run();
     }
 }
