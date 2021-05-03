e02754e9e2779ace0a465b5ce8aba0e7c5debfe0
Pouria-Empire <arefijamal.pouria@gmail.com>
2021-05-03 20:55:55 +0430

    potOfGreed done

--- src/main/java/models/SpellAndTrap/PotOfGreed.java
+++ src/main/java/models/SpellAndTrap/PotOfGreed.java
@@ -1,10 +1,18 @@
 package models.SpellAndTrap;
 
+import controllers.Battelfield.Battlefield;
+import models.Card;
 import models.CardStufs.Type;
+import models.Duelist;
+import view.UserInterface;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Objects;
 
 public class PotOfGreed extends SpellAndTrap implements Serializable {
+    private static Duelist turn;
+    private static Duelist opponent;
 
     public PotOfGreed (String name, Type cardType, String description, int price, String icon, String status){
         super(name, cardType, description, price, icon, status);
@@ -16,8 +24,29 @@ public class PotOfGreed extends SpellAndTrap implements Serializable {
                 ((PotOfGreed)object).getIcon(), ((PotOfGreed)object).getStatus());
     }
 
-//    @Override
-//    public void action() {
-//
-//    }
+    @Override
+    public void action() {
+        turn = Battlefield.getTurn();
+        opponent = Battlefield.getOpponent();
+        //checking not null
+        if(!Objects.isNull(turn) && !Objects.isNull(opponent)){
+            if(getSizeOfHand()<=4){
+                //add two card
+                turn.field.hand.add(turn.field.deck.get(0));
+                turn.field.deck.remove(0);
+                turn.field.hand.add(turn.field.deck.get(0));
+                turn.field.deck.remove(0);
+            }
+            else
+                UserInterface.printResponse("you dont have enough space in your hand");
+
+        }
+    }
+    private int getSizeOfHand(){
+        int count = 0;
+        for(int i=0;i<6;i++){
+            if(turn.field.hand.get(i) != null) count++;
+        }
+        return count;
+    }
 }
