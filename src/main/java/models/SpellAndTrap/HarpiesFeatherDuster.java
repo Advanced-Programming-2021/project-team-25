db30802d7a59eb2e7e75f52aa4a104545c435730
Pouria-Empire <arefijamal.pouria@gmail.com>
2021-05-04 14:02:18 +0430

    did

--- src/main/java/models/SpellAndTrap/HarpiesFeatherDuster.java
+++ src/main/java/models/SpellAndTrap/HarpiesFeatherDuster.java
@@ -1,10 +1,15 @@
 package models.SpellAndTrap;
 
+import controllers.Battelfield.Battlefield;
+import models.Card;
 import models.CardStufs.Type;
+import models.Duelist;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 public class HarpiesFeatherDuster extends SpellAndTrap implements Serializable {
+    private static Duelist opponent;
 
     public HarpiesFeatherDuster (String name, Type cardType, String description, int price, String icon, String status){
         super(name, cardType, description, price, icon, status);
@@ -16,8 +21,15 @@ public class HarpiesFeatherDuster extends SpellAndTrap implements Serializable {
                 ((HarpiesFeatherDuster)object).getIcon(), ((HarpiesFeatherDuster)object).getStatus());
     }
 
-//    @Override
-//    public void action() {
-//
-//    }
+    @Override
+    public void action() {
+        opponent = Battlefield.getTurn();
+        //checking not null
+        if(!Objects.isNull(opponent)){
+            for(Card card : opponent.field.spellTrapZone){
+                opponent.field.graveYard.add(card);
+                opponent.field.spellTrapZone.set(opponent.field.spellTrapZone.indexOf(card) , null);
+            }
+        }
+    }
 }
