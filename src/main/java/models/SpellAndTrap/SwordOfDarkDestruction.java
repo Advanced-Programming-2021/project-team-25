da8cc5e037832a2dae7b554078986a9a2952b472
nazari <X.LordAliX@yahoo.com>
2021-05-05 18:32:07 +0430

    Sword of dark destruction spell added

--- src/main/java/models/SpellAndTrap/SwordOfDarkDestruction.java
+++ src/main/java/models/SpellAndTrap/SwordOfDarkDestruction.java
@@ -1,8 +1,13 @@
 package models.SpellAndTrap;
 
+import controllers.Battelfield.Battlefield;
 import models.CardStufs.Type;
+import models.Duelist;
+import models.Monster.Monster;
+import view.UserInterface;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class SwordOfDarkDestruction extends SpellAndTrap implements Serializable {
 
@@ -16,8 +21,55 @@ public class SwordOfDarkDestruction extends SpellAndTrap implements Serializable
                 ((SwordOfDarkDestruction)object).getIcon(), ((SwordOfDarkDestruction)object).getStatus());
     }
 
-//    @Override
-//    public void action() {
-//
-//    }
+    @Override
+    public void action() {
+        Duelist turn = Battlefield.getTurn();
+
+        int counter = 0;
+        ArrayList<Monster> numbers = new ArrayList<>();
+        for (int i = 0; i<5; ++i){
+            if (turn.field.monsterZone.get(i) != null){
+                Monster temp = (Monster) turn.field.monsterZone.get(i);
+                if (!targetedMonsters.contains(temp) && temp.getMonsterType().equals("Fiend")) {
+                    counter += 1;
+                    numbers.add(temp);
+                }
+                else if (!targetedMonsters.contains(temp) && temp.getMonsterType().equals("Spellcaster")) {
+                    counter += 1;
+                    numbers.add(temp);
+                }
+            }
+        }
+
+        if (counter == 0)
+            UserInterface.printResponse("You can't activate this spell because you dont have any monster " +
+                    "with type of Fiend or Spellcaster");
+        else{
+            UserInterface.printResponse("Now select one of these card(s):");
+            for (Monster number : numbers)
+                UserInterface.printResponse(number.getName() + ":" + number.getDescription());
+
+
+            String name = " ";
+            while (true){
+                String command = UserInterface.getUserInput();
+                for (Monster number : numbers)
+                    if (number.getName().equals(command)){
+                        name = command;
+                        spellActivate(number);
+                        break;
+                    }
+                if (name.equals(" "))
+                    UserInterface.printResponse("Please insert a valid monster name.");
+            }
+        }
+    }
+
+    public void spellActivate (Monster monster){
+        targetedMonsters.add(monster);
+        monster.setAttack(monster.getAttack() + 400);
+        monster.setDefence(monster.getDefence() - 200);
+    }
+
+
 }
