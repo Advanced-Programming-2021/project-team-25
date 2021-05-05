587e21a97e5edc56ebc9bf36cc65b79164fd249a
Pouria-Empire <arefijamal.pouria@gmail.com>
2021-05-05 19:39:16 +0430

    MessengerOfPeace done

--- src/main/java/models/SpellAndTrap/MessengerOfPeace.java
+++ src/main/java/models/SpellAndTrap/MessengerOfPeace.java
@@ -1,10 +1,19 @@
 package models.SpellAndTrap;
 
+import controllers.Battelfield.Battlefield;
+import controllers.Battelfield.Phase;
+import models.Card;
 import models.CardStufs.Type;
+import models.Duelist;
+import models.Monster.Monster;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class MessengerOfPeace extends SpellAndTrap implements Serializable {
+    //get turn and opponent
+    Duelist opponent = Battlefield.getOpponent();
+    Duelist turn = Battlefield.getTurn();
 
     public MessengerOfPeace (String name, Type cardType, String description, int price, String icon, String status){
         super(name, cardType, description, price, icon, status);
@@ -16,8 +25,29 @@ public class MessengerOfPeace extends SpellAndTrap implements Serializable {
                 ((MessengerOfPeace)object).getIcon(), ((MessengerOfPeace)object).getStatus());
     }
 
-//    @Override
-//    public void action() {
-//
-//    }
+    @Override
+    public void action() {
+        //get phase
+        Phase phase = Battlefield.getPhase();
+        if(phase.equals(Phase.STANDBY_PHASE))
+            turn.LP-=100;
+        //monster wit attack >= 1500 cant attack
+        for(Card card : opponent.field.monsterZone){
+            if(((Monster)card).getAttack()>=1500){
+                targetedMonsters.add((Monster)card);
+                ((Monster)card).setCanAttack(false);
+            }
+        }
+    }
+
+    @Override
+    public void removeSpellOrTrap(String name) {
+        //clearing things done
+        for(Card card : opponent.field.monsterZone){
+            if(((Monster)card).getAttack()>=1500){
+                targetedMonsters.remove(card);
+                ((Monster)card).setCanAttack(true);
+            }
+        }
+    }
 }
