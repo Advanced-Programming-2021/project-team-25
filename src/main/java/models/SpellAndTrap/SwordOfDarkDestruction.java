2787e1e5ebd398c19950f7538622703260cd2d34
nazari <X.LordAliX@yahoo.com>
2021-05-06 09:53:17 +0430

    Sword of dark destruction completed.

--- src/main/java/models/SpellAndTrap/SwordOfDarkDestruction.java
+++ src/main/java/models/SpellAndTrap/SwordOfDarkDestruction.java
@@ -1,8 +1,13 @@
 package models.SpellAndTrap;
 
+import controllers.Battelfield.Battlefield;
 import models.CardStufs.Type;
+import models.Duelist;
+import models.Monster.Monster;
+import view.UserInterface;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class SwordOfDarkDestruction extends SpellAndTrap implements Serializable {
 
@@ -18,8 +23,55 @@ public class SwordOfDarkDestruction extends SpellAndTrap implements Serializable
 
     @Override
     public void action() {
+        Duelist turn = Battlefield.getTurn();
 
+        int counter = 0;
+        ArrayList<Monster> trueMonsters = new ArrayList<>();
+        for (int i = 0; i<5; ++i){
+            if (turn.field.monsterZone.get(i) != null){
+                Monster sample = (Monster) turn.field.monsterZone.get(i);
+                if (!targetedMonsters.contains(sample) && targetedMonsters.size() == 0 &&
+                        (sample.getMonsterType().equals("Fiend") || sample.getMonsterType().equals("Spellcaster"))) {
+                    trueMonsters.add(sample);
+                    counter += 1;
+                }
+            }
+        }
+
+        if (targetedMonsters.size() > 0)
+            UserInterface.printResponse("This spell has already equiped a monster.");
+        else if (counter == 0)
+            UserInterface.printResponse("You don't have monster with type of Fiend or Spellcaster.");
+        else{
+            UserInterface.printResponse("Now select one of these monsters to equip it");
+            for (Monster trueMonster : trueMonsters) {
+                UserInterface.printResponse(trueMonster.getName() + ":" + trueMonster.getDescription());
+            }
+            String name = " ";
+            while (true){
+                String command = UserInterface.getUserInput();
+                for (Monster trueMonster : trueMonsters) {
+                    if (trueMonster.getName().equals(command)) {
+                        name = command;
+                        equipMonster(trueMonster);
+                        break;
+                    }
+                }
+                if (name.equals(" "))
+                    UserInterface.printResponse("Insert a valid name please.");
+                else
+                    break;
+            }
+        }
     }
+
+
+    public void equipMonster (Monster monster){
+        targetedMonsters.add(monster);
+        monster.setAttack(monster.getAttack() + 400);
+        monster.setDefence(monster.getDefence() - 200);
+    }
+
 
     @Override
     public void removeSpellOrTrap(String name) {
