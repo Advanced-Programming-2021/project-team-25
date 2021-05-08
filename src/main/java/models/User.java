03fa1f540426c3a1a832b939d693bff3ce025ee4
Pouria-Empire <arefijamal.pouria@gmail.com>
2021-05-08 21:19:25 +0430

    bug fixed

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -35,7 +35,7 @@ public class User implements Serializable {
 
     public static User getUserByNickName(String nickName) {
         for(User user : users)
-            if(user.getUsername().equals(nickName)) return user;
+            if(user.getNickName().equals(nickName)) return user;
 
         return null;
     }
