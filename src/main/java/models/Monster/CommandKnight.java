b30bd2b61f0384070b9dff72a9e3248abc1f7144
nazari <X.LordAliX@yahoo.com>
2021-05-12 22:41:50 +0430

    command knight completed

--- src/main/java/models/Monster/CommandKnight.java
+++ src/main/java/models/Monster/CommandKnight.java
@@ -3,6 +3,8 @@ package models.Monster;
 import controllers.Battelfield.Battlefield;
 import models.Card;
 import models.CardStufs.Type;
+import models.User;
+import view.UserInterface;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -24,13 +26,36 @@ public class CommandKnight extends Monster implements Serializable {
     }
 
 
-//    @Override
-//    public void removeMonster(Battlefield battlefield) {
-//
-//    }
+    @Override
+    public void removeMonster(Battlefield battlefield) {
+        for (Card targetedMonster : targetedMonsters) {
+            Monster temp = (Monster) targetedMonster;
+            temp.setAttack(temp.getAttack() - 400);
+        }
+        super.removeMonster(battlefield);
+    }
 
-//    @Override
-//    public int defenceFunc(Battlefield battlefield) {
-//
-//    }
+    @Override
+    public int defenceFunc(Battlefield battlefield) {
+//        return super.defenceFunc(battlefield);
+        int counter = 0;
+        for (int i = 0; i<5; ++i){
+            if (battlefield.getOpponent().field.monsterZone.get(i) != null &&
+            battlefield.getOpponent().field.monsterZone.get(i) != this){
+                counter += 1;
+            }
+        }
+
+        if (counter == 0)
+            return super.defenceFunc(battlefield);
+        else{
+            UserInterface.printResponse("There are more monsters on your opponents monster zone" +
+                    "so you can't attack this monster.");
+            return 10;
+        }
+
+
+    }
+
+
 }
