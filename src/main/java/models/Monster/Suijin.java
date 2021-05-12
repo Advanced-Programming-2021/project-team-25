3247140088bb7413e1b75afcab1aa27d9030bcba
nazari <X.LordAliX@yahoo.com>
2021-05-12 16:55:39 +0430

    suijin added

--- src/main/java/models/Monster/Suijin.java
+++ src/main/java/models/Monster/Suijin.java
@@ -1,10 +1,12 @@
 package models.Monster;
 
+import controllers.Battelfield.Battlefield;
 import models.CardStufs.Type;
 
 import java.io.Serializable;
 
 public class Suijin extends Monster implements Serializable {
+    private int counter = 0;
 
     public Suijin (String name, Type cardType, String description, int price, int level, String attribute,
                               String monsterType, String cardTypeInExel, int attack, int defence){
@@ -19,8 +21,19 @@ public class Suijin extends Monster implements Serializable {
                 ((Suijin)object).getAttack(), ((Suijin)object).getDefence());
     }
 
-//    @Override
-//    public void action() {
-//
-//    }
+
+    @Override
+    public int defenceFunc(Battlefield battlefield) {
+        if (counter == 0){
+            counter += 1;
+            int a = super.defenceFunc(battlefield);
+            if(a == 1) battlefield.attackingMonster.removeMonster(battlefield);
+            return a;
+        }
+        else{
+            return super.defenceFunc(battlefield);
+        }
+    }
+
+
 }
