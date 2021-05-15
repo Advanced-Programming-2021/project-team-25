7f80ee0a6db909deba5305f25d7ae4226487e4b8
Pouria-Empire <arefijamal.pouria@gmail.com>
2021-05-15 13:43:43 +0430

    register unittest added

---
+++ src/main/java/UnitTests/LoginRegisterTest.java
@@ -0,0 +1,43 @@
+package UnitTests;
+
+import controllers.Regex;
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Objects;
+
+import static org.junit.Assert.assertEquals;
+
+import static controllers.menues.LoginMenu.createNewUser;
+
+public class LoginRegisterTest {
+    @Test
+    public void userRegisterTest() throws IOException {
+        final String output = "src/main/java/UnitTests/RegisterTestOutput.txt";
+        final String input = "src/main/java/UnitTests/RegisterTest.txt";
+        final String expectedOutput = "src/main/java/UnitTests/RegisterExpected.txt";
+        PrintStream fileStream = new PrintStream(output);
+        System.setOut(fileStream);
+        try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                createNewUser(Regex.getMatcher(line,Regex.userCreate),false);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        List<String> file1 = Files.readAllLines(Paths.get(output));
+        List<String> file2 = Files.readAllLines(Paths.get(expectedOutput));
+
+        assertEquals(file1.size(), file2.size());
+
+        for(int i = 0; i < file1.size(); i++) {
+            System.out.println("Comparing line: " + i);
+            assertEquals(file1.get(i), file2.get(i));
+        }
+
+    }
+}
