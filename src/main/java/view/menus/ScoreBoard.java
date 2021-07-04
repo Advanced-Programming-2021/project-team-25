5940211dba9fba285cc8275bcd62a1e20eff4422
nazari <X.LordAliX@yahoo.com>
2021-07-04 19:12:41 +0430

    Scoreboard completed

--- src/main/java/view/menus/ScoreBoard.java
+++ src/main/java/view/menus/ScoreBoard.java
@@ -249,38 +249,74 @@ class ShowAllUsers {
             hBox.getChildren().add(imageView);
 
             Button buttonRank = new Button(String.valueOf(ranks.get(i)));
-            buttonRank.setStyle("-fx-background-color: #090a0c," +
-                    "linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%)," +
-                    "linear-gradient(#20262b, #191d22)," +
-                    "radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));" +
-                    "-fx-background-radius: 5,4,3,5; -fx-background-insets: 0,1,2,0; -fx-text-fill: white;" +
-                    "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
-                    "-fx-font-family: \"Arial\"; -fx-text-fill: linear-gradient(white, #d0d0d0);" +
-                    "-fx-font-size: 12px; -fx-padding: 10 20 10 20;");
+            if (i >= 20) {
+                buttonRank.setStyle("-fx-background-color: #090a0c," +
+                        "linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%)," +
+                        "linear-gradient(#20262b, #191d22)," +
+                        "radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));" +
+                        "-fx-background-radius: 5,4,3,5; -fx-background-insets: 0,1,2,0; -fx-text-fill: white;" +
+                        "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
+                        "-fx-font-family: \"Arial\"; -fx-text-fill: linear-gradient(white, #d0d0d0);" +
+                        "-fx-font-size: 12px; -fx-padding: 10 20 10 20;");
+            }
+            else{
+                buttonRank.setStyle("-fx-background-color: #FFA500," +
+                        "linear-gradient(#FFA500 0%, #FFA500 20%, #FFA500 100%)," +
+                        "linear-gradient(#FFA500, #FFA500)," +
+                        "radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));" +
+                        "-fx-background-radius: 5,4,3,5; -fx-background-insets: 0,1,2,0; -fx-text-fill: white;" +
+                        "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
+                        "-fx-font-family: \"Arial\"; -fx-text-fill: linear-gradient(white, #d0d0d0);" +
+                        "-fx-font-size: 12px; -fx-padding: 10 20 10 20;");
+            }
             hBox.getChildren().add(buttonRank);
 
 
             Button buttonName = new Button(names.get(i));
-            buttonName.setStyle("-fx-background-color: #090a0c," +
-                    "linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%)," +
-                    "linear-gradient(#20262b, #191d22)," +
-                    "radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));" +
-                    "-fx-background-radius: 5,4,3,5; -fx-background-insets: 0,1,2,0; -fx-text-fill: white;" +
-                    "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
-                    "-fx-font-family: \"Arial\"; -fx-text-fill: linear-gradient(white, #d0d0d0);" +
-                    "-fx-font-size: 12px; -fx-padding: 10 20 10 20;");
+            if (i >= 20) {
+                buttonName.setStyle("-fx-background-color: #090a0c," +
+                        "linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%)," +
+                        "linear-gradient(#20262b, #191d22)," +
+                        "radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));" +
+                        "-fx-background-radius: 5,4,3,5; -fx-background-insets: 0,1,2,0; -fx-text-fill: white;" +
+                        "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
+                        "-fx-font-family: \"Arial\"; -fx-text-fill: linear-gradient(white, #d0d0d0);" +
+                        "-fx-font-size: 12px; -fx-padding: 10 20 10 20;");
+            }
+            else {
+                buttonName.setStyle("-fx-background-color: #FFA500," +
+                        "linear-gradient(#FFA500 0%, #FFA500 20%, #FFA500 100%)," +
+                        "linear-gradient(#FFA500, #FFA500)," +
+                        "radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));" +
+                        "-fx-background-radius: 5,4,3,5; -fx-background-insets: 0,1,2,0; -fx-text-fill: white;" +
+                        "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
+                        "-fx-font-family: \"Arial\"; -fx-text-fill: linear-gradient(white, #d0d0d0);" +
+                        "-fx-font-size: 12px; -fx-padding: 10 20 10 20;");
+            }
             hBox.getChildren().add(buttonName);
 
 
             Button buttonScore = new Button(String.valueOf(scores.get(i)));
-            buttonScore.setStyle("-fx-background-color: #090a0c," +
-                    "linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%)," +
-                    "linear-gradient(#20262b, #191d22)," +
-                    "radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));" +
-                    "-fx-background-radius: 5,4,3,5; -fx-background-insets: 0,1,2,0; -fx-text-fill: white;" +
-                    "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
-                    "-fx-font-family: \"Arial\"; -fx-text-fill: linear-gradient(white, #d0d0d0);" +
-                    "-fx-font-size: 12px; -fx-padding: 10 20 10 20;");
+            if (i >= 20) {
+                buttonScore.setStyle("-fx-background-color: #090a0c," +
+                        "linear-gradient(#38424b 0%, #1f2429 20%, #191d22 100%)," +
+                        "linear-gradient(#20262b, #191d22)," +
+                        "radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));" +
+                        "-fx-background-radius: 5,4,3,5; -fx-background-insets: 0,1,2,0; -fx-text-fill: white;" +
+                        "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
+                        "-fx-font-family: \"Arial\"; -fx-text-fill: linear-gradient(white, #d0d0d0);" +
+                        "-fx-font-size: 12px; -fx-padding: 10 20 10 20;");
+            }
+            else{
+                buttonScore.setStyle("-fx-background-color: #FFA500," +
+                        "linear-gradient(#FFA500 0%, #FFA500 20%, #FFA500 100%)," +
+                        "linear-gradient(#FFA500, #FFA500)," +
+                        "radial-gradient(center 50% 0%, radius 100%, rgba(114,131,148,0.9), rgba(255,255,255,0));" +
+                        "-fx-background-radius: 5,4,3,5; -fx-background-insets: 0,1,2,0; -fx-text-fill: white;" +
+                        "-fx-effect: dropshadow( three-pass-box , rgba(0,0,0,0.6) , 5, 0.0 , 0 , 1 );" +
+                        "-fx-font-family: \"Arial\"; -fx-text-fill: linear-gradient(white, #d0d0d0);" +
+                        "-fx-font-size: 12px; -fx-padding: 10 20 10 20;");
+            }
             hBox.getChildren().add(buttonScore);
 
             gridPane.add(hBox, 0, i);
