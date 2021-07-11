c237411808e7eee4123ffc2d8a512ba5792a79ee
Pouria-Empire <arefijamal.pouria@gmail.com>
2021-07-11 13:04:24 +0430

    initialize fixed !

--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -1,11 +1,12 @@
 package view;
 
 
-
+import controllers.Constants.Initialize;
 
 public class MainView {
 
     public static void main(String[] args) {
+        Initialize.init();
         API api = API.getInstance();
         api.run();
     }
